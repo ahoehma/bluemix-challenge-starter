ed3c58655c73fd31f64a2e61f8d548297670226c
Andreas Höhmann <andreas.hoehmann@gmx.de>
2015-10-04 19:12:17 +0200

    [add] challenge #1 - ask user for name an show it

--- src/main/java/org/bluemix/challenge/MyUI.java
+++ src/main/java/org/bluemix/challenge/MyUI.java
@@ -5,13 +5,20 @@ import javax.servlet.annotation.WebServlet;
 import com.vaadin.annotations.Theme;
 import com.vaadin.annotations.VaadinServletConfiguration;
 import com.vaadin.annotations.Widgetset;
+import com.vaadin.data.Property;
+import com.vaadin.data.Property.ValueChangeEvent;
+import com.vaadin.data.Property.ValueChangeListener;
+import com.vaadin.data.util.ObjectProperty;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.server.VaadinServlet;
 import com.vaadin.ui.Button;
 import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.FormLayout;
 import com.vaadin.ui.Label;
+import com.vaadin.ui.TextField;
 import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
+import com.vaadin.ui.Window;
 
 /**
  *
@@ -20,22 +27,42 @@ import com.vaadin.ui.VerticalLayout;
 @Widgetset("org.bluemix.challenge.MyAppWidgetset")
 public class MyUI extends UI {
 
-    @Override
+	@Override
     protected void init(VaadinRequest vaadinRequest) {
-        final VerticalLayout layout = new VerticalLayout();
+        final VerticalLayout layout = new VerticalLayout(); 
         layout.setMargin(true);
         setContent(layout);
 
-        Button button = new Button("Click Me");
+        final Button button = new Button("Click Me");
         button.addClickListener(new Button.ClickListener() {
             @Override
             public void buttonClick(ClickEvent event) {
-                layout.addComponent(new Label("Thank you for clicking"));
-            }
+            	final Property<String> name = new ObjectProperty<String>("", String.class);
+            	final TextField textField = new TextField("What's your name?", name);
+            	textField.focus();
+            	textField.setRequired(true);
+            	final Window nameInputWindow = new Window();
+            	nameInputWindow.setModal(true);
+            	nameInputWindow.setClosable(false);
+            	nameInputWindow.setResizable(false);
+            	FormLayout content = new FormLayout(textField);
+				content.setMargin(true);
+				nameInputWindow.setContent(content);
+            	nameInputWindow.center();
+            	nameInputWindow.setWidth(400, Unit.PIXELS);
+				addWindow(nameInputWindow);
+				textField.addValueChangeListener(new ValueChangeListener() {
+					
+					@Override
+					public void valueChange(ValueChangeEvent event) {
+						nameInputWindow.close();
+						layout.addComponent(new Label(name.getValue() + "! Thank you for clicking"));
+					}
+				});
+            };
         });
         layout.addComponent(button);
-
-    }
+    } 
 
     @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
     @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
